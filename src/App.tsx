adb9c71e7f4d3b9497a7952995d0adda1f37b2cf
Prabu <prabu.mgks@gmail.com>
2022-05-09 13:34:24 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="bg-almostWhite"></div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"],
+      },
+      colors: {
+        almostWhite: "hsl(0, 0%, 98%)",
+        mediumGray: "hsl(0, 0%, 41%)",
+        almostBlack: "hsl(0, 0%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+};
