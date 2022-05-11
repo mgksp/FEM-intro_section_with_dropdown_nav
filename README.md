8428d56099590d8a768adc60f1da2b7eca3783ff
Prabu <prabu.mgks@gmail.com>
2022-05-11 12:58:37 +0530

    readme file and screenshots

---
+++ README.md
@@ -0,0 +1,27 @@
+# FEM - Intro Section with Dropdown Navigation
+
+### Links
+
+[Challenge from FrontendMentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5)\
+[Live site here]()
+
+### Built with
+
+- vite
+- react
+- typescript
+- tailwindcss
+- framer-motion
+
+### Screenshots
+
+<table>
+  <tr>
+    <td>Mobile Version</td>
+    <td>Desktop Version</td>
+  </tr>
+  <tr valign="top">
+    <td><img src="./screenshots/mobile.png" alt="mobile version" /></td>
+    <td><img src="./screenshots/desktop.png" alt="desktop version" /></td>
+  </tr>
+</table>
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,38 @@ import { Header, Hero } from "./components";
 
 function App() {
   return (
-    <>
+    <div className="relative min-h-screen">
       <Header />
       <Hero />
-    </>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
+const Footer = () => {
+  return (
+    <div className="text-xs text-center w-full py-1 text-mediumGray absolute bottom-0 left-0">
+      Challenge by&nbsp;
+      <a
+        className="text-sm font-bold text-almostBlack"
+        href="https://www.frontendmentor.io?ref=challenge"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by&nbsp;
+      <a
+        className="text-sm font-bold text-almostBlack"
+        href="https://www.github.com/mgksp"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Prabu
+      </a>
+      .
+    </div>
+  );
+};
