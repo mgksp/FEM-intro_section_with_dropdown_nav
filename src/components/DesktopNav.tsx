5aea88afbb1904801a38446b3180fc54d890ac95
Prabu <prabu.mgks@gmail.com>
2022-05-31 15:02:54 +0530

    feature added

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
+import { SetStateAction, useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
-
-import { menus } from "../utils/enums";
 
 import iconArrUp from "../images/icon-arrow-up.svg";
 import iconArrDown from "../images/icon-arrow-down.svg";
@@ -15,99 +13,22 @@ export default function DesktopNav() {
   const [showFeatures, setShowFeatures] = useState(false);
   const [showCompany, setShowCompany] = useState(false);
 
-  const handleDropdownMenuToggle = (menu: menus) => {
-    if (menu === menus.features) {
-      setShowCompany(false);
-      setShowFeatures((prev) => !prev);
-    }
-
-    if (menu === menus.company) {
-      setShowFeatures(false);
-      setShowCompany((prev) => !prev);
-    }
-  };
-
   return (
     <nav
       aria-label="desktop-navigation"
       className="hidden md:flex text-mediumGray justify-between items-center w-full"
     >
       <div className="flex gap-8">
-        <div className="relative">
-          <a
-            href="#"
-            className="flex items-center gap-1 hover:text-almostBlack"
-            onClick={() => handleDropdownMenuToggle(menus.features)}
-          >
-            Features{" "}
-            <img
-              className={showFeatures ? "hidden" : "block"}
-              src={iconArrDown}
-              alt=""
-            />
-            <img
-              className={showFeatures ? "block" : "hidden"}
-              src={iconArrUp}
-              alt=""
-            />
-          </a>
-          <motion.div
-            initial={{ y: "-20rem" }}
-            animate={{ y: showFeatures ? 0 : "-20rem" }}
-            className="absolute bg-white rounded-xl shadow-nav p-6 -bottom-[750%] right-0 w-40"
-          >
-            <ul className="flex gap-2 flex-col">
-              <li className="flex items-center gap-2 cursor-pointer hover:text-almostBlack">
-                <img className="w-4" src={iconTodo} alt="" />
-                <p>Todo&nbsp;List</p>
-              </li>
-              <li className="flex items-center gap-2 cursor-pointer hover:text-almostBlack">
-                <img className="w-4" src={iconCalendar} alt="" />
-                <p>Calendar</p>
-              </li>
-              <li className="flex items-center gap-2 cursor-pointer hover:text-almostBlack">
-                <img className="w-4" src={iconReminders} alt="" />
-                <p>Reminders</p>
-              </li>
-              <li className="flex items-center gap-2 cursor-pointer hover:text-almostBlack">
-                <img className="w-4" src={iconPlanning} alt="" />
-                <p>Planning</p>
-              </li>
-            </ul>
-          </motion.div>
-        </div>
-        <div className="relative">
-          <a
-            href="#"
-            className="flex items-center gap-1 hover:text-almostBlack"
-            onClick={() => handleDropdownMenuToggle(menus.company)}
-          >
-            Company{" "}
-            <img
-              className={showCompany ? "hidden" : "block"}
-              src={iconArrDown}
-              alt=""
-            />
-            <img
-              className={showCompany ? "block" : "hidden"}
-              src={iconArrUp}
-              alt=""
-            />
-          </a>
-          <motion.div
-            initial={{ y: "-20rem" }}
-            animate={{ y: showCompany ? 0 : "-20rem" }}
-            className="absolute bg-white rounded-xl shadow-nav p-6 -bottom-[620%] left-0 w-32"
-          >
-            <ul className="flex gap-2 flex-col">
-              <li className="cursor-pointer hover:text-almostBlack">History</li>
-              <li className="cursor-pointer hover:text-almostBlack">
-                Our Team
-              </li>
-              <li className="cursor-pointer hover:text-almostBlack">Blog</li>
-            </ul>
-          </motion.div>
-        </div>
+        <DropDownMenu
+          title="Features"
+          navMenuState={showFeatures}
+          setNavMenuState={setShowFeatures}
+        />
+        <DropDownMenu
+          title="Company"
+          navMenuState={showCompany}
+          setNavMenuState={setShowCompany}
+        />
         <a href="#" className="hover:text-almostBlack">
           Careers
         </a>
@@ -124,3 +45,71 @@ export default function DesktopNav() {
     </nav>
   );
 }
+
+interface DropDownMenuProps {
+  title: string;
+  navMenuState: boolean;
+  setNavMenuState: React.Dispatch<SetStateAction<boolean>>;
+}
+const DropDownMenu = ({
+  title,
+  navMenuState,
+  setNavMenuState,
+}: DropDownMenuProps) => {
+  const node = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function closeNavMenu(evt: MouseEvent) {
+      if (
+        navMenuState &&
+        node.current &&
+        !node.current.contains(evt.target as Node)
+      ) {
+        setNavMenuState!(false);
+      }
+    }
+
+    document.addEventListener("mousedown", closeNavMenu);
+
+    return () => {
+      document.removeEventListener("mousedown", closeNavMenu);
+    };
+  }, [navMenuState]);
+
+  return (
+    <div ref={node}>
+      <a
+        href="#"
+        className="flex items-center gap-1 hover:text-almostBlack"
+        onClick={() => {
+          setNavMenuState!((prev) => !prev);
+        }}
+      >
+        {title}
+        <img
+          className={navMenuState ? "hidden" : "block"}
+          src={iconArrDown}
+          alt=""
+        />
+        <img
+          className={navMenuState ? "block" : "hidden"}
+          src={iconArrUp}
+          alt=""
+        />
+      </a>
+      <div className="relative">
+        <motion.div
+          initial={{ y: "-20rem" }}
+          animate={{ y: navMenuState ? 0 : "-20rem" }}
+          className="absolute bg-white rounded-xl shadow-nav p-6 top-4 left-0 w-32"
+        >
+          <ul className="flex gap-2 flex-col">
+            <li className="cursor-pointer hover:text-almostBlack">History</li>
+            <li className="cursor-pointer hover:text-almostBlack">Our Team</li>
+            <li className="cursor-pointer hover:text-almostBlack">Blog</li>
+          </ul>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
